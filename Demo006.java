1f26f3f945204959b4cdaaff3c3af94c27dc55aa
aadithya729 <adiadithya392@gmail.com>
2025-01-03 10:13:01 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "adithya";
+        String pwd = "adithya@123";
+        while ((name == "adithya")&&(pwd == "adithya@123")){
+            System.out.println("Welcome adithya");
+            break;
+        }
+        }
+}
