d010901ccf2013e26425b0680d50dd3f58badec2
aadithya729 <adiadithya392@gmail.com>
2025-02-07 10:25:46 +0530

    Add files via upload

---
+++ Demo063.java
@@ -0,0 +1,13 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        
+        int firstElement = arr[0];
+        
+        int lastElement = arr[arr.length - 1];
+        int  add = firstElement + lastElement;
+        
+        System.out.println("addition of first and last element is: "+add);
+
+}
+}
